7106729ee30e41c588ce761beb1fa90bcadefc6d
dongdaxiaodong <32518351+dongdaxiaodong@users.noreply.github.com>
2024-02-18 18:42:21 -0600

    Add files via upload

---
+++ mulog.r
@@ -0,0 +1,10 @@
+logb 1: loga 1 logb 1: 1
+loga 1 logY 2: logc 1 logYp 1 loga 1 : 1000
+logc 2 : logc 1 : 1000
+loga 1 : logW 1 : 100
+logYp 1 : logY 1 : 10
+logc 1 : logm 1 : 10
+muX 1: mua 1: 1
+mua 1 logc 1: mua 1 muYp 1 muZ 1 : 100000
+mua 1 : muW 1 : 10000
+muYp 1 : logc 1 : 1000
